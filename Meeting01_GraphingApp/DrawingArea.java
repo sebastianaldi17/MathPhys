9f341c963eb1abd3092a6b35b7d7fa57c1bb7d09
Livia Lohanda <chocolate.coffee@outlook.com>
2019-06-02 21:59:01 +0700

    Add codes for Meeting 01

---
+++ Meeting01_GraphingApp/DrawingArea.java
@@ -0,0 +1,155 @@
+package Meeting01_GraphingApp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+class DrawingArea extends JPanel {
+    private boolean draw = false;
+    private int width;
+    private int height;
+    private int originX;        // the origin points (0, 0)
+    private int originY;
+    private double scaleX;      //scaling the canvas according to lengthX & lengthY
+    private double scaleY;
+    private double lengthX;     // how many numbers shown along absis and ordinate
+    private double lengthY;
+    private double currentX;    // current X-point, the Y is retrieved from
+    private double increment;   // controlling detail
+    private final static int MAX_POINTS = 1000;    // in case the function is a loop, or the thread runs for far too long
+    private ArrayList<Point2D.Double> points1 = new ArrayList<>();
+    private ArrayList<Point2D.Double> points2 = new ArrayList<>();
+    private Image drawingArea;
+    private Thread animator;    // thread to draw the graph
+
+    // setup the drawing area
+    public DrawingArea(int width, int height, int cpWidth) {
+        super(null);
+        this.width = width - cpWidth;
+        this.height = height;
+        setBounds(cpWidth, 0, this.width, this.height);
+        originX = this.width/2;
+        originY = this.height/2;
+        setBackground(Color.white);
+        drawingArea = createImage(this.width, this.height);
+    }
+
+    // functions to draw on the screen
+    private double function1(double x) {
+        return 4*x*x-16;
+    }
+
+    private double function2(double x) {
+        return 2*x+1;
+    }
+
+    // start drawing graph
+    public void beginDrawing(double lengthX, double lengthY, double startX, double increment) {
+        // retrieve data
+        this.lengthX = lengthX + 1;
+        this.lengthY = lengthY + 1;
+        this.scaleX = (double) (width - originX) / lengthX;
+        this.scaleY = (double) (height - originY) / lengthY;
+        this.currentX = startX;
+        this.increment = increment;
+
+        // trigger drawing process
+        draw = true;
+        drawingArea = createImage(width, height);
+        animator = new Thread(this::eventLoop);
+        animator.start();
+    }
+
+    private void eventLoop() {
+        while(draw) {
+            update();
+            render();
+            printScreen();
+            try {
+                animator.sleep(10);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        // clean up the thread
+        System.out.println("stopping");
+        draw = false;
+        points1.clear();
+        points2.clear();
+        try {
+            animator.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // add points to be drawn
+    private void update()
+    {
+        // while there is still a need to draw
+        if(currentX < lengthX && currentX > -lengthX && points1.size() < MAX_POINTS)
+        {
+            System.out.println("checking");
+            currentX = currentX + increment;
+            points1.add(new Point2D.Double(currentX, function1(currentX)));
+            points2.add(new Point2D.Double(currentX, function2(currentX)));
+        }
+        else {      // cleanup for the next thread
+            draw = false;
+        }
+    }
+
+    // draw the buffered area before showing to screen
+    private void render() {
+        if (drawingArea != null) {
+            //get graphics of the image where coordinate and function will be drawn
+            Graphics g = drawingArea.getGraphics();
+            g.setColor(Color.BLACK);
+
+            //draw the x-axis and y-axis
+            g.drawLine(0, originY, width, originY);
+            g.drawLine(originX, 0, originX, height);
+
+            //print numbers on the x-axis and y-axis, based on the scale
+            for (int i = 0; i < lengthX; i++) {
+                g.drawString(Integer.toString(i), (int) (originX + (i * scaleX)), originY);
+                g.drawString(Integer.toString(-1 * i), (int) (originX + (-i * scaleX)), originY);
+            }
+            for (int i = 0; i < lengthY; i++) {
+                g.drawString(Integer.toString(-1 * i), originX, (int) (originY + (i * scaleY)));
+                g.drawString(Integer.toString(i), originX, (int) (originY + (-i * scaleY)));
+            }
+
+            // draw the lines
+            for (int i = 0; i < points1.size() - 1; i++) {
+                g.drawLine((int) (originX + points1.get(i).x * scaleX), (int) (originY - points1.get(i).y * scaleY),
+                        (int) (originX + points1.get(i + 1).x * scaleX), (int) (originY - points1.get(i + 1).y * scaleY));
+                g.drawLine((int) (originX + points2.get(i).x * scaleX), (int) (originY - points2.get(i).y * scaleY),
+                        (int) (originX + points2.get(i + 1).x * scaleX), (int) (originY - points2.get(i + 1).y * scaleY));
+            }
+        }
+    }
+
+    // print the previously buffered area to screen
+    private void printScreen()
+    {
+        try
+        {
+            Graphics g = getGraphics();
+            if(drawingArea != null && g != null)
+            {
+                g.drawImage(drawingArea, 0, 0, null);
+            }
+
+            // Sync the display on some systems.
+            // (on Linux, this fixes event queue problems)
+            Toolkit.getDefaultToolkit().sync();
+            g.dispose();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Graphics error: " + ex);
+        }
+    }
+}
---
+++ Meeting01_GraphingApp/GraphingApp.java
@@ -0,0 +1,110 @@
+package Meeting01_GraphingApp;
+
+/*
+    Matfis pertemuan 01
+    Simple graphing app
+    All UI-elements are measured in pixels, while the functions are drawn in units and angle is in radian
+    Please write every decimals with dot (.) not comma (,)
+
+    Explain how the graphing app works, which controls the coordinates shown, what functions are drawn.
+    Generally, in screen with double-buffer system, there would be one layer to be displayed and another layer to be drawn on
+    Thus in event loop, there are three steps:
+    - Update the module (NPC's movement, player's action, etc)
+    - Render the new updates in the drawing layer
+    - Change current display layer with the previously drawn layer.
+
+    TODO:
+     1. Play with the app. What does each field do? What's the difference if you change one field? What if you use smaller/bigger increment factor?
+     2. Change the function into another continuous linear/polynomial function
+     3. Change the function into other functions with discontinuity, such as 1/x. What happened?
+     4. Change the function into trigonometric functions or any function that needs java.util.Math library
+     5. Change the function and fields into parametric functions
+     6. Change the coordinates into polar coordinates
+*/
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+
+class GraphingApp {
+    private JFrame frame;
+
+    // members of control panel
+    private JPanel controlPanel;
+    private DrawingArea drawingArea;
+    private JTextField fieldLengthX;
+    private JTextField fieldLengthY;
+    private JTextField fieldBegin;
+    private JTextField fieldIncrement;
+
+    private int cpWidth = 270;      // set control panel's width
+
+    public GraphingApp() {
+        // setup the frame
+        frame = new JFrame("Graphing App");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        // setup control panel itself
+        controlPanel = new JPanel(new GridLayout(5, 2, 5, 0));
+        createControlPanel();
+        controlPanel.setBounds(0, 0, cpWidth, 300);
+        frame.add(controlPanel);
+
+        // setup drawing area
+        drawingArea = new DrawingArea(frame.getWidth(), frame.getHeight(), cpWidth);
+        frame.add(drawingArea);
+    }
+
+    // add members to control panel
+    private void createControlPanel() {
+        // the x-axis would be shown from -lengthX to lengthX
+        JLabel labelLengthX = new JLabel("Length of X axis");
+        controlPanel.add(labelLengthX);
+        fieldLengthX = new JTextField(2);
+        controlPanel.add(fieldLengthX);
+
+        // the y-axis would be shown from -lengthY to lengthY
+        JLabel labelLengthY = new JLabel("Length of Y axis");
+        controlPanel.add(labelLengthY);
+        fieldLengthY = new JTextField(2);
+        controlPanel.add(fieldLengthY);
+
+        // function calculation will start from startX
+        JLabel labelBegin = new JLabel("Start point from X");
+        controlPanel.add(labelBegin);
+        fieldBegin = new JTextField(2);
+        controlPanel.add(fieldBegin);
+
+        // the lesser the number, the more detailed it will become
+        JLabel labelIncrement = new JLabel("Incremental factor");
+        controlPanel.add(labelIncrement);
+        fieldIncrement = new JTextField(2);
+        controlPanel.add(fieldIncrement);
+
+        // clicking the button will start the animation
+        JButton button = new JButton("Begin graphing");
+        button.addActionListener(e -> sendValuesToDrawer());
+        controlPanel.add(button);
+    }
+
+    // get values from text fields
+    private void sendValuesToDrawer() {
+        double lengthX = Double.parseDouble(fieldLengthX.getText());
+        double lengthY = Double.parseDouble(fieldLengthY.getText());
+        double begin = Double.parseDouble(fieldBegin.getText());
+        double increment = Double.parseDouble(fieldIncrement.getText());
+        drawingArea.beginDrawing(lengthX, lengthY, begin, increment);
+        drawingArea.beginDrawing(lengthX, lengthY, begin, increment);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(GraphingApp::new);
+    }
+}
